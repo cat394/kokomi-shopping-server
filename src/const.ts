19c2f589fc295e8eed52774075ff249a41caf5c0
cat394 <keyring1231@gmail.com>
2025-01-09 08:47:07 +0900

    chore: add line brakes to make it easier to read

--- src/const.ts
+++ src/const.ts
@@ -3,5 +3,7 @@ import path from "path";
 export const ROOT_DIRECTORY = path.resolve();
 
 export const MAX_NUMBER_OF_PRODUCT_IMAGES = 5;
+
 export const MAX_NUMBER_OF_LIST_TO_RETRIEVE = 30;
+
 export const MAX_NUMBER_OF_CART_ITEMS = 150;
